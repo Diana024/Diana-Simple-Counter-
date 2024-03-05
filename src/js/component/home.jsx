5875a6df151496e62f38083a9889b7579e109298
Diana024 <154326533+Diana024@users.noreply.github.com>
2024-03-05 20:16:15 +0000

    Diana-Contador-Simple

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,23 +4,27 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	//Aqui hago toda las consultas
+	console.log(props);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	//Lo que devuleven los props.
+		<div className="m-3 d-flex bg-dark justify-content-center pt-4 pb-4 text font-family-serif">
+			<>
+			<h1 className="text-center text-white p-4 bg-gradient fs-1 border border-end-0 border border-light fs-1 rounded mx-2 px-3">
+				<i class="far fa-clock Light link"></i>
+			</h1>
+			<h1 className="text-center text-white p-4 bg-gradient fs-1 border border-start-0 border border-light fs-1 rounded mx-2 px-3">{props.digito6}</h1>
+			<h1 className="text-center text-white p-4 bg-gradient fs-1 border border-start-0 border border-light fs-1 rounded mx-2 px-3">{props.digito5}</h1>
+			<h1 className="text-center text-white p-4 bg-gradient fs-1 border border-start-0 border border-light fs-1 rounded mx-2 px-3">{props.digito4}</h1>
+			<h1 className="text-center text-white p-4 bg-gradient fs-1 border border-start-0 border border-light fs-1 rounded mx-2 px-3">{props.digito3}</h1>
+			<h1 className="text-center text-white p-4 bg-gradient fs-1 border border-start-0 border border-light fs-1 rounded mx-2 px-3">{props.digito2}</h1>
+			<h1 className="text-center text-white p-4 bg-gradient fs-1 border border-start-0 border border-light fs-1 rounded mx-2 px-3">{props.digito1}</h1>
+			</>
 		</div>
+		
 	);
 };
+
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,51 @@ import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let num1 = 0;
+let num2 = 0;
+let num3 = 0;
+let num4 = 0;
+let num5 = 0;
+let num6 = 0;
+
+setInterval(function() {
+    console.log (num1++, num2, num3, num4, num5, num6); 
+    if (num1 === 10) {
+         num1 = 0;
+         num2++; 
+    }
+        if (num2 === 10) {
+            num2 = 0;
+            num3++;
+        } 
+            if (num3 === 10) {
+                num3 = 0;
+                num4++;
+            } 
+                if (num4 === 10) {
+                    num4 = 0;
+                    num5++;
+                } 
+                if (num5 === 10) {
+                    num5 = 0;
+                    num6++;
+                } 
+                if (num6 === 10) {
+                    num6 = 0;
+                    num1++;
+                } 
+                   
+
+    //render your react application
+    ReactDOM.render(<Home digito1={num1} digito2={num2} digito3={num3} digito4={num4} digito5={num5} digito6={num6}/>, document.querySelector("#app"));
+}, 1000);
+
+
+//1. pasar un props <Componente props="valr"/>
+//2. recibir un props donde se declara el componente
+
+
+// ReactDOM.render(<Home digito1={num1} digito2={num2} digito3={num3} digito4={num4} digito5={num5} digito6={num6}/>, document.querySelector("#app"));
+// }, 1000);
+
